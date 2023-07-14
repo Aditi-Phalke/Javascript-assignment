e1340a5c8b4ac01bc87064e18e37abcf41fdbe1a
Aditi Phalke <aditiphalke1759@gmail.com>
2023-07-14 20:36:49 +0530

    EMI Calculator is completed

--- EMICalculator/app.js
+++ EMICalculator/app.js
@@ -1,8 +1,8 @@
-function calculateEMI(){
-    let amount=document.getElementById('amount')
-    let month=document.getElementById('month')
-    let rate=document.getElementById('rate')
-    const interset=(amount.value * (rate.value * 0.01)) / month.value
-    let EMI=((amount.value / month.value) + interset).toFixed(2)
-    document.getElementById('EMI').innerText="EMI = " +EMI
+function calculateEMI() {
+    let amount = document.getElementById('amount')
+    let month = document.getElementById('month')
+    let rate = document.getElementById('rate')
+    const interset = (amount.value * (rate.value * 0.01)) / month.value
+    let EMI = ((amount.value / month.value) + interset).toFixed(2)
+    document.getElementById('EMI').innerText = "EMI = " + EMI
 }
